75571130188ec38a8df22f97612410b04601b752
Bharat Kathi <bharat1031@gmail.com>
2024-08-28 14:32:28 -0700

    banner test lol

---
+++ config/banner_test.go
@@ -0,0 +1,10 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestPrintStartupBanner(t *testing.T) {
+	// This test simply calls the function to ensure it runs without panicking
+	PrintStartupBanner()
+}
