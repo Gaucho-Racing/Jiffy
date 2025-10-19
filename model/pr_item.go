f1ffb2ab95647bdd251667a6ed586801967e3f24
austinchan3678 <austinchan@ucsb.edu>
2025-10-19 05:51:54 -0700

    rename ;

---
+++ model/pr_item.go
@@ -0,0 +1,18 @@
+package model
+
+import "time"
+
+type PurchaseRequestItem struct {
+	ID                int       `json:"id" gorm:"primaryKey;autoIncrement"`
+	PurchaseRequestID int       `json:"purchase_request_id"`
+	URL               string    `json:"url"`
+	Name              string    `json:"name"`
+	UnitPriceCents    int       `json:"unit_price_cents"`
+	Quantity          int       `json:"quantity"`
+	CreatedAt         time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt         time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
+
+func (PurchaseRequestItem) TableName() string {
+	return "purchase_request_item"
+}
